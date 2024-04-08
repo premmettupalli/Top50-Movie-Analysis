a042329850616acc312f448d66a6ea24ceb2cc37
premmettupalli <pmettupalli@trentu.ca>
2024-04-08 13:12:07 -0400

    Final 5.0

--- app.R
+++ app.R
@@ -5,30 +5,39 @@ library(dplyr)
 library(plotly)
 library(readxl)
 
-# Reading the dataset
+# Reading the data-set
 movies <- readxl::read_xlsx("movies_data.xlsx")
 
 # Defining UI for the dashboard
 ui <- fluidPage(
+  #title of the dash board
   titlePanel("Movie Statistics"),
+  #designing the sidebar layout and sidebar panel for hosting filtering options 
   sidebarLayout(
     sidebarPanel(
       width = 3,
-      helpText("Create interactive plots based on movie data"),
+      helpText("Filtering options"),
+      #Genre Filter
       selectInput("genre_filter", "Filter by Genre:",
                   choices = c("All", unique(movies$Genre))),
+      #Director Filter
       selectInput("director_filter", "Filter by Director:",
                   choices = c("All", unique(movies$Director))),
+      #Year Filter
       sliderInput("year_filter", "Filter by Year:",
                   min = min(movies$Year), max = max(movies$Year),
                   value = c(min(movies$Year), max(movies$Year)), step = 1),
+      #Rating filter
       sliderInput("rating_filter", "Filter by Rating:",
                   min = min(movies$Rating), max = max(movies$Rating),
-                  value = c(min(movies$Rating), max(movies$Rating)), step = 0.1),
+                  value = c(min(movies$Rating), max(movies$Rating)), 
+                  step = 0.1),
+      #Clear Button 
       actionButton("clear_filters", "Clear All Filters"),
       hr(),
       helpText("Note: Not all plots may have valid data for all categories.")
     ),
+    #designing layout of the main panel 
     mainPanel(
       width = 9,
       tabsetPanel(
@@ -54,7 +63,7 @@ ui <- fluidPage(
 # Defining the server logic for the dashboard 
 server <- function(input, output, session) {
   
-  # Adding Genre, Director,Year, and Ratings filters to the dashboard 
+  # Adding functionality to the Genre, Director,Year, and Ratings filters 
   filtered_data <- reactive({
     filtered <- movies
     if (input$genre_filter != "All") {
@@ -65,16 +74,19 @@ server <- function(input, output, session) {
     }
     filtered <- filtered %>%
       filter(Year >= input$year_filter[1], Year <= input$year_filter[2]) %>%
-      filter(Rating >= input$rating_filter[1], Rating <= input$rating_filter[2])
+      filter(Rating >= input$rating_filter[1], 
+             Rating <= input$rating_filter[2])
     return(filtered)
   })
   
-  # adding fliters clearing button
+  # adding functionality to the filter clearing button
   observeEvent(input$clear_filters, {
     updateSelectInput(session, "genre_filter", selected = "All")
     updateSelectInput(session, "director_filter", selected = "All")
-    updateSliderInput(session, "year_filter", value = c(min(movies$Year), max(movies$Year)))
-    updateSliderInput(session, "rating_filter", value = c(min(movies$Rating), max(movies$Rating)))
+    updateSliderInput(session, "year_filter", value = c(min(movies$Year), 
+                                                        max(movies$Year)))
+    updateSliderInput(session, "rating_filter", value = c(min(movies$Rating), 
+                                                          max(movies$Rating)))
   })
   
   #Plot of Top 20 Movies by Votes
@@ -96,9 +108,11 @@ server <- function(input, output, session) {
       summarise(Total_Votes = sum(Votes)) %>%
       arrange(desc(Total_Votes)) %>%
       slice(1:20)
-    p <- ggplot(top_directors, aes(x = reorder(Director, Total_Votes), y = Total_Votes)) +
+    p <- ggplot(top_directors, aes(x = reorder(Director, Total_Votes), 
+                                   y = Total_Votes)) +
       geom_bar(stat = "identity", fill = "salmon") +
-      labs(x = "Director", y = "Total Votes", title = "Top 20 Directors by Total Votes") +
+      labs(x = "Director", y = "Total Votes", 
+           title = "Top 20 Directors by Total Votes") +
       theme(axis.text.x = element_text(angle = 45, hjust = 1))
     ggplotly(p)
   })
@@ -111,8 +125,10 @@ server <- function(input, output, session) {
       mutate(Proportion = Total / sum(Total)) %>%
       arrange(desc(Proportion))
     
-    p <- plot_ly(data = genre_proportions, labels = ~Genre, values = ~Total, type = 'pie',
-                 marker = list(colors = rainbow(length(genre_proportions$Genre))),
+    p <- plot_ly(data = genre_proportions, labels = ~Genre, values = ~Total,
+                 type = 'pie',
+                 marker = list(colors = 
+                                 rainbow(length(genre_proportions$Genre))),
                  textposition = 'inside',
                  textinfo = 'percent+label')
     p <- p %>% layout(title = 'Proportions of Different Genres')
@@ -134,10 +150,12 @@ server <- function(input, output, session) {
   
   # Correlation plot between Budgets in million and Profit in million
   output$correlation_plot <- renderPlotly({
-    p <- ggplot(filtered_data(), aes(x = Budget_in_Million, y = Profit_in_Mil)) +
+    p <- ggplot(filtered_data(), aes(x = Budget_in_Million,
+                                     y = Profit_in_Mil)) +
       geom_point() +
       geom_smooth(method = "lm", se = FALSE) +
-      labs(x = "Budget (in million)", y = "Profit (in million)", title = "Correlation between Budget and Profit")
+      labs(x = "Budget (in million)", y = "Profit (in million)", 
+           title = "Correlation between Budget and Profit")
     ggplotly(p)
   })
 }
